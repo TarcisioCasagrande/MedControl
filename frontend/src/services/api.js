6362c8c71a59f2627daba40a31084d9aa144be99
Tarcisio <tarcisio.casagrande@gmail.com>
2026-05-15 17:43:51 -0300

    Atualização final

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://medcontrol-4.onrender.com/api',
+  baseURL: 'http://localhost:5179/api',
 });
 
 api.interceptors.request.use((config) => {
